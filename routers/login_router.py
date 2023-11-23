704efb110f967404dca278a66e7dfcf9ff1854e6
YURIY <yury.myworkmail@gmail.com>
2023-11-23 16:05:43 +0300

    0.0.57 jwt 1 token not work

--- routers/login_router.py
+++ routers/login_router.py
@@ -9,6 +9,8 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 router = APIRouter(tags=["login"])
+
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 SECRET_KEY = "your-secret-key"
@@ -29,6 +31,7 @@ class UserOut(UserIn):
     is_active: bool
 
 
+
 @router.post("/token", response_model=Token)
 def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
     # Replace this with actual user lookup and password verification
@@ -38,12 +41,50 @@ def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    # Generate a JWT token
-    token_data = {
+    # Generate JWT access token
+    access_token_expires = timedelta(days=5)
+    access_token_data = {
         "sub": form_data.username,
-        "exp": datetime.utcnow() + timedelta(minutes=30),
+        "exp": datetime.utcnow() + access_token_expires,
     }
-    token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
-    response.set_cookie(key="access_token", value=f"Bearer {token}", httponly=True)
-    return {"access_token": token, "token_type": "bearer"}
+    access_token = jwt.encode(access_token_data, SECRET_KEY, algorithm=ALGORITHM)
 
+    # Generate JWT refresh token
+    refresh_token_expires = timedelta(days=7)
+    refresh_token_data = {
+        "sub": form_data.username,
+        "exp": datetime.utcnow() + refresh_token_expires,
+    }
+    refresh_token = jwt.encode(refresh_token_data, SECRET_KEY, algorithm=ALGORITHM)
+
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
+    response.set_cookie(key="refresh_token", value=f"Bearer {refresh_token}", httponly=True)
+    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+
+@router.post("/token/refresh", response_model=Token)
+def refresh_token(response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
+    # Decode the token, allowing it to be expired up to 30 minutes
+    try:
+        payload = jwt.decode(form_data.username, SECRET_KEY, algorithms=[ALGORITHM], options={"verify_exp": False})
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid access token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    if datetime.utcnow() > payload["exp"] + timedelta(minutes=30):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token expired more than 30 minutes ago",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # Generate a new JWT access token
+    access_token_expires = timedelta(minutes=15)
+    access_token_data = {
+        "sub": payload["sub"],
+        "exp": datetime.utcnow() + access_token_expires,
+    }
+    access_token = jwt.encode(access_token_data, SECRET_KEY, algorithm=ALGORITHM)
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
+    return {"access_token": access_token, "token_type": "bearer"}
--- routers/payment_router.py
+++ routers/payment_router.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from typing import List
 from pydantic import BaseModel
 from peewee import DoesNotExist
 from models.db_model import Payment
 from auth.login import get_current_user
-
+from fastapi.security import OAuth2PasswordBearer
+import jwt
 router = APIRouter(tags=["payment"])
 
 class UserIn(BaseModel):
@@ -25,11 +26,37 @@ class PaymentOut(PaymentIn):
     id: int
     status: str
 
-
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
 
 # return all payments
-@router.get("/payments", response_model=List[PaymentOut])
-def read_payments(user: UserOut = Depends(get_current_user), limit: int = 20, offset: int = 0):
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token/refresh")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    print(token)
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        print(str)
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid authentication credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        # Replace this with actual user lookup in your database
+        user = UserOut(username=username, id=1, is_active=True)
+        return user
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+@router.get("/paymentsss", response_model=List[PaymentOut])
+# def read_payments(user: UserOut = Depends(get_current_user), limit: int = 20, offset: int = 0):
+def read_payments(limit: int = 20, offset: int = 0):
     payments = Payment.select().offset(offset).limit(limit)
     return [PaymentOut(**payment.__data__) for payment in payments]
 
